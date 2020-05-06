1911dced2418b5f83a5e32b372754c8881869dd2
cosmmiike <mikhail.shaym@gmail.com>
2020-05-07 00:12:50 +0200

    Visualization

--- evolution_3.py
+++ evolution_3.py
@@ -1,23 +1,29 @@
 from random import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+
 
 class World:
     def __init__(self):
-        self.birth_rate = 1  # chance of a new spontaneously born creation
+        self.birth_rate = 1  # chance of a new spontaneously born creation is 100 %
         self.creations = []
 
     def evolve(self):
-        self.reproduction()
-        self.death()
         self.spontaneous_birth()
+        self.replication()
+        self.death()
 
     def spontaneous_birth(self):
         if random() < self.birth_rate:
             self.creations.append(Creation())
 
-    def reproduction(self):
+    def replication(self):
         new_creations = []
         for creation in self.creations:
-            baby = creation.reproduce()
+            baby = creation.replicate()
             if baby is not None:
                 new_creations.append(baby)
         self.creations += new_creations
@@ -30,10 +36,10 @@ class World:
 
 class Creation:
     def __init__(self):
-        self.death_rate = 0.5  # chance of death is set to 50 %
-        self.repoduction_rate = 0.5  # chance of reproduction is set to 50 %
+        self.death_rate = 0.1  # chance of death is set to 10 %
+        self.repoduction_rate = 0  # chance of replication is set to 0
 
-    def reproduce(self):
+    def replicate(self):
         if random() < self.repoduction_rate:
             return Creation()
         return None
@@ -44,19 +50,29 @@ class Creation:
         return False
 
 
+def show_analytics(data):
+    df = pd.DataFrame(data, columns=['Day', 'Population', 'Average'])
+    df.set_index('Day', inplace=True)
+    sns.lineplot(data=df)
+    plt.show()
+
+
 def main():
-    days = 10
+    days = 365
     world = World()
 
-    print()
-    print('    Day  |   Population  ')
+    print('\n    Day  |   Population  ')
     print('-------------------------')
 
+    data = []
+    accum = 0
     for day in range(1, days + 1):
         world.evolve()
+        accum += len(world.creations)
+        data.append((day, len(world.creations), accum / day))
         print('{:>7}  | {:>12}  '.format(day, len(world.creations)))
 
-    print()
+    show_analytics(data)
 
 
 if __name__ == '__main__':
